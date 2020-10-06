19f4c38b1644bef0d76a3d98146db16d843a8faf
M Yusril <34540627+shansekai@users.noreply.github.com>
2020-10-06 15:21:33 +0700

    Create id.js

---
+++ id.js
@@ -0,0 +1,260 @@
+exports.textTnC = () => {
+    return `
+Source code / bot ini merupakan program open-source (gratis) yang ditulis menggunakan Javascript, kamu dapat menggunakan, menyalin, memodifikasi, menggabungkan, menerbitkan, mendistribusikan, mensublisensikan, dan atau menjual salinan dengan tanpa menghapus author utama dari source code / bot ini.
+
+Dengan menggunakan source code / bot ini maka anda setuju dengan Syarat dan Kondisi sebagai berikut:
+- Source code / bot tidak menyimpan data anda di server kami.
+- Source code / bot tidak bertanggung jawab atas sticker yang anda buat dari bot ini serta video, gambar maupun data lainnya yang anda dapatkan dari Source code / bot ini.
+- Source code / bot tidak boleh digunakan untuk layanan yang bertujuan/berkontribusi dalam: 
+    • seks / perdagangan manusia
+    • perjudian
+    • perilaku adiktif yang merugikan 
+    • kejahatan
+    • kekerasan (kecuali jika diperlukan untuk melindungi keselamatan publik)
+    • pembakaran hutan / penggundulan hutan
+    • ujaran kebencian atau diskriminasi berdasarkan usia, jenis kelamin, identitas gender, ras, seksualitas, agama, kebangsaan
+
+Source Code BOT : https://github.com/YogaSakti/imageToSticker
+NodeJS WhatsApp library: https://github.com/open-wa/wa-automate-nodejs
+
+Best regards, Yoga Sakti.`
+}
+
+exports.textMenu = (pushname) => {
+    return `
+Hai kak ${pushname}, jangan lupa subscribe channel *Youtube dan follow Instagram* aku untuk mengetahui update bot.
+Instagram : https://www.instagram.com/yusril.id_
+Youtube : https://www.youtube.com/channel/UCQF4ZMDKr4bmAsA7_6rewgA
+
+Berikut dibawah ini menu yang tersedia pada *Sansekai BOT*
+
+*NOTE : KALIAN TIDAK PERLU MEMAKAI TANDA [ DAN ]*
+
+┏☾ Donate Commands ☽
+╿
+┠❥ *#donasi*
+╿
+┷┯ ☾ Others Commands ☽
+   ╽
+   ┠❥ *#iklan*
+   ┠❥ *#ownerbot*
+   ┠❥ *#masukgrup*
+   ┠❥ *#rules*
+   ┠❥ *#server*
+   ┠❥ *#sticker*
+   ┠❥ *#gifsticker [link giphy only]*
+   ┠❥ *#memesticker [teks | teks]*
+   ┠❥ *#neko*
+   ┠❥ *#pokemon*
+   ┠❥ *#inu*
+   ┠❥ *#dadu*
+   ┠❥ *#quotemaker [teks] | [author] | [theme]*
+   ┠❥ *#igstalk [@username]*
+   ┠❥ *#wiki [query]*
+   ┠❥ *#anime [judul anime]*
+   ┠❥ *#teksmaker [optional]*
+   ┠❥ *#qrcode [teks]*
+   ┠❥ *#teksmaker [teks | teks]*
+   ┠❥ *#binary [teks]*
+   ┠❥ *#translate [kode bhs] [reply pesan]*
+   ┠❥ *#news*
+   ┠❥ *#infogempa*
+   ┠❥ *#reverseteks [teks]*
+   ┠❥ *#ceklokasi [kirim lokasi lalu reply]*
+   ┠❥ *#resi [nama kurir]*
+   ┠❥ *#tts [kode bhs] [teks]*
+   ┠❥ *#waifu*
+   ┠❥ *#husbu*
+   ┠❥ *#ptl*
+   ┠❥ *#wallanime*
+   ┠❥ *#wallpaper*
+   ┠❥ *#randomanime*
+   ┠❥ *#randomnekonime*
+   ┠❥ *#randomtrapnime*
+   ┠❥ *#meme*
+   ┠❥ *#quotes*
+   ┠❥ *#qanime*
+   ┠❥ *#wait*
+   ┠❥ *#lirik [optional]*
+   ╿
+┯┷ ☾ Downloader Commands ☽
+╽
+┠❥ *#twt [linkTwitter]*
+┠❥ *#tiktok [linkTiktok]*
+┠❥ *#fb [linkFb]*
+┠❥ *#ig [linkIg]*
+┠❥ *#ytmp4 [linkyt]*
+┠❥ *#ytmp3 [linkyt]*
+╿
+┷┯ ☾ Group Commands ☽
+   ╽
+   ┠❥ *#kick @tagmember*
+   ┠❥ *#add 628xxxxxxxxxx*
+   ┠❥ *#promote @tagmember*
+   ┠❥ *#demote @tagadmin*
+   ┠❥ *#tagall*
+   ┠❥ *#adminList*
+   ┠❥ *#ownergrup*
+   ┠❥ *#bye*
+   ┠❥ *#grupinfo*
+   ┠❥ *#del [replyChatBot]*
+   ┠❥ *#kickallmember*
+   ┠❥ *#nsfw [enable|disable]*
+   ╿
+┯┷ 🔞 NSFW Command 🔞
+╽
+┠❥ *#lewd*
+┠❥ *#fetish [tag]*
+┠❥ *#randomhentai*
+┠❥ *#randomnsfwneko*
+╿
+┷┯ ☾ Owner Command ☽
+  ╿
+  ┠❥ *#bc [teks]*
+  ┠❥ *#leaveall*
+  ┠❥ *#clearall*
+  ┠❥ *#listblock*
+  ┠❥ *#getsschat*
+  ╿
+  ╰╼❥ Kirim perintah *#readme* untuk mengetahui fungsi dan cara penggunaan perintah di atas, WAJIB BACA!!.`
+}
+
+exports.textAdmin = () => {
+    return `
+⚠ [ *Admin Group Only* ] ⚠ 
+Berikut adalah beberapa fitur admin grup yang ada pada bot ini!
+
+➥ *#kick @tagmember*
+Mengeluarkan member nakal yang ada di group mu
+*Note* : Bot tidak bisa mengeluarkan admin/owner group!
+
+➥ *#add [nomor]*
+Menambahkan member di group mu
+Contoh : *#add 62858xxxx*
+
+➥ *#demote @tagadmin*
+Menurunkan pangkat admin menjadi member
+
+➥ *#promote @tagmember*
+Menaikkan pangkat member menjadi admin
+
+➥ *#bye*
+Bot keluar dari group ini.(hanya admin grup yang bisa memakai perintah ini)
+
+➥ *#tagall*
+Mention semua member group termasuk admin
+
+➥ *#del*
+menghapus pesan bot (balas pesan bot dengan #del)
+
+➥ *#kickAll*
+Mengeluarkan semua member group!
+*Note* : Admin group tidak terkena kick)`
+}
+
+exports.textDonasi = (pushname) => {
+    return `
+Hai Kak ${pushname}, Kamu Bisa Support Bot Agar Tetap Terus *Aktif* Dan Membantu Kami Membayar *Server* Dengan *Berdonasi* ke:
+
+Dana: 081283468899
+OVO: 081283468899
+GOPAY: 081283468899
+PULSA TELKOMSEL: 081283468899
+
+*Menerima donasi berapapun jumlahnya 🙏 Terima Kasih.*`
+}
+
+exports.textRules = () => {
+    return `
+*RULES BAGI PENGGUNA BOT*
+    
+➥ Tolong Gunakan Delay Jangan Spam Saat Menggunakan Bot, Mentang Mentang Gratis Diborong semua.
+(Kecuali yang donasi boleh spam hehehe.)
+➥ Call/VC Bot Auto Block.
+➥ Jangan Call/VC Bot Kalau Tidak aktif, Ganggu Bgt Gblk.
+➥ Bot tidak aktif 24 jam, jadi tergantung kalau ownernya lagi ada waktu botnya juga on.
+
+*Konsekuensi Bila Melanggar Rules*
+Bot Akan Memblokir Kamu Atau Keluar Dari Grup Yang Kamu Kelola.
+
+Rules ini untuk kenyamanan semua yang memakai
+bot ini `
+}
+
+exports.textReadme = (pushname) => {
+    return `
+Hai kak ${pushname}, mohon baca tata cara dibawah ini supaya kamu paham menggunakan botnya.
+ 
+*[link giphy only]*
+mengubah gif menjadi sticker bergerak *(hanya bisa menggunakan link dari giphy)*
+Contoh : *#gifsticker https://media.giphy.com/media/JUvI2c1ddyzkwK4RlV/giphy.gif*
+
+*#teksmaker [optional]*
+Untuk membuat meme dengan teks atas dan bawah
+Penggunaan: kirim gambar dengan caption *#teksmaker ini atas | ini bawah* atau juga bisa dengan membalas gambar yang sudah ada.
+
+*[kode bhs] [reply pesan]*
+Untuk mengartikan pesan menjadi bahasa yang ditentukan.
+Penggunaan: Balas/quote/reply pesan yang ingin kamu translate dengan *#translate id* <- id adalah kode bahasa. kode bahasa dapat dilihat di *https://bit.ly/33FVldE*
+ 
+*[kirim lokasi lalu reply #ceklokasi]*
+Cek lokasi penyebaran covid-19 di daerah sekitarmu (kelurahan).
+Penggunaan: kirimkan lokasimu lalu balas/quote/reply lokasi yang kamu kirim dengan *#ceklokasi*
+
+*[nama kurir]*
+jne, pos, tiki, wahana, jnt, rpx, sap, sicepat, pcp, jet, dse, first, ninja, lion, idl, rex.
+Contoh : *#resi pos 12345678910*
+ 
+*[linkTwitter]* Diisi dengan link Twitter yang valid tanpa tanda “[” dan “]”
+Contoh : *#twt https://www.twitter.com/blablabla*
+ 
+*[linktiktok]* Diisi dengan link Tiktok yang valid tanpa tanda “[” dan “]”
+Contoh : *#tiktok https://vt.tiktok.com/blablabla/*
+
+*[linkFb]* Diisi dengan link Facebook yang valid tanpa tanda “[” dan “]”
+Contoh : *#fb https://m.facebook.com/story.php?story_fbid=1222361854807374&id=100011007121639*
+
+*[linkIg]* Diisi dengan link Instagram yang valid tanpa tanda “[” dan “]”
+Contoh : *#ig https://www.instagram.com/p/CBpzFmypUU0/?igshid=1t3q1cxs714jk*
+
+*[linkYt]* Diisi dengan link Youtube yang valid tanpa tanda “[” dan “]”
+Contoh : *#ytmp4 https://youtu.be/FXe8o2PsvcI*
+
+*[kode bhs]* Diisi dengan kode bahasa, contoh *id*, *en*, *ar*, *jp*, *ru*, *ko*. Dan *[teks]* Diisi dengan teks yang ingin di jadikan voice, Masih sama seperti di atas tanpa tanda “[” dan “]”
+Contoh : *#tts id Test*
+Note : Max 1000 huruf
+
+*[optional]* Diisi dengan teks|title lirik lagu, tanpa tanda “[” dan “]”.
+Contoh : *#lirik aku bukan boneka*
+
+*[tag]*
+Yang tersedia: *armpits*, *feets*, *thighs*, *booty*, *boobs*, *necks*, *belly*, *sideboobs*, dan *ahegao*.
+Contoh : *#fetish boobs*
+`
+}
+
+exports.textIklan = (pushname) => {
+    return `
+Hai kak ${pushname}, selamat datang di menu iklan.
+
+Disini saya mau menawarkan *Youtube Premium* dibeli ya kak🙏
+
+📺 *SANSEKAI SHOP PROMO* 📺
+
+Youtube Premium Hanya Rp.10.000/Bulan 100% Legal 
+
+*Kelebihan Youtube Premium:*
+- Menonton Tanpa Iklan
+- Bisa Menyimpan Video
+- Kualitas Video Lebih Bagus
+- Akses Youtube Original
+- Akses Youtube Music
+- Video Bisa Diputar Latar Belakang
+- Bukan Memakai Aplikasi Mod
+- Dll
+
+Bila Berminat Beli, Bisa  Chat Wa kesini: https://wa.me/6281283468899
+`
+}
+
+
